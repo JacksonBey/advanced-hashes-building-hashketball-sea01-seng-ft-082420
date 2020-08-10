0b48eec49415bba9802e1f9976d958e55c5079b3
Jackson Beytebiere <jacksongrowson@gmail.com>
2020-08-10 18:10:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
     home: {
       team_name: "Brooklyn Nets",
       colors: ["Black","White"],
-      players: [{
+      players: [
         {:player_name => "Alan Anderson",
           number: 0,
           shoe: 16,
@@ -61,8 +61,8 @@ def game_hash
         away: {
       team_name: "Charlotte Hornets",
       colors: ["Turquoise","Purple"],
-      players: [{
-        "Jeff Adrien" => {
+      players: [
+        {:player_name => "Jeff Adrien",
           number: 4,
           shoe: 18,
           points: 10,
@@ -72,7 +72,7 @@ def game_hash
           blocks: 7,
           slam_dunks: 2
         },
-        "Bismack Biyombo" => {
+        {:player_name => "Bismack Biyombo",
           number: 0,
           shoe: 16,
           points: 12,
@@ -82,7 +82,7 @@ def game_hash
           blocks: 15,
           slam_dunks: 10
         },
-        "DeSagna Diop" => {
+        {:player_name => "DeSagna Diop",
           number: 2,
           shoe: 14,
           points: 24,
@@ -92,7 +92,7 @@ def game_hash
           blocks: 5,
           slam_dunks: 5
         },
-        "Ben Gordon" => {
+        {:player_name => "Ben Gordon",
           number: 8,
           shoe: 15,
           points: 33,
@@ -102,7 +102,7 @@ def game_hash
           blocks: 1,
           slam_dunks: 0
         },
-        "Kemba Walker" => {
+        {:player_name => "Kemba Walker",
           number: 33,
           shoe: 15,
           points: 6,
